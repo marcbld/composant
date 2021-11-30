e30c05bbde2fcc06d7bdbd03aaa15ec3456eda3e
Marc Blanchard <marc.blanchard600@gmail.com>
2021-11-30 12:48:01 +0100

    architecture v1

---
+++ RangeSlider/src/main/java/fr/caro_marc/rangeslider/composants/bar/leftBar/leftBar.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.caro_marc.composants.leftBar;
+
+/**
+ *
+ * @author blanchma
+ */
+public class leftBar {
+    
+}
--- RangeSlider/src/main/java/fr/caro_marc/rangeslider/controler/RangeSliderControler.java
+++ RangeSlider/src/main/java/fr/caro_marc/rangeslider/controler/RangeSliderControler.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fr.caro_marc.rangeslider;
+package fr.caro_marc.rangeslider.controler;
 
 /**
  *
  * @author blanchma
  */
-public class RangeSlider {
+public class RangeSliderControler {
     
 }
